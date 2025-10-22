2b15d3900bdbbfdfaddf14fe8a72d0f7752d2c0c
minjin0610 <minjin3755@gmail.com>
2025-10-22 17:06:21 +0900

    chore : AwsS3Config.java 디렉토리 이동

--- src/main/java/com/ktb/community/config/aws/AwsS3Config.java
+++ src/main/java/com/ktb/community/config/aws/AwsS3Config.java
@@ -1,4 +1,4 @@
-package com.ktb.community.common.aws.config;
+package com.ktb.community.config.aws;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
